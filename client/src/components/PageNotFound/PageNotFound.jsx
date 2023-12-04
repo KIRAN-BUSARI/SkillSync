6aa9de7713e10a2c45271233900d33aa8902f874
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-12-04 14:09:32 +0530

    added the footer header and pagenotfound file

---
+++ client/src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PageNotFound() {
+    return (
+        <div className="h-[90vh] mx-auto w-full max-w-7xl flex justify-center items-center text-4xl text-orange-600">
+        {" "}
+            404 Page Not Found <span className="text-5xl">🧐...</span>
+        </div>
+    );
+}
+
+export default PageNotFound;
