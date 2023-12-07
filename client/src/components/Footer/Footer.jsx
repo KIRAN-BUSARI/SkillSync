01c0a3e0ef50acdfd9cb423abe2ab9b6fe3f4fdd
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-12-07 18:50:57 +0530

    Fixed the FOoter

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -80,7 +80,7 @@ function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
+                        © 2023{" "}
                         <a href="#" className="hover:underline">
                             SkillSync
                         </a>
