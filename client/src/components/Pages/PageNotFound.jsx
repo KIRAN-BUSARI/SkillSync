a3be42253c7716c0353dc86c6a36f753c2cc4ec2
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-12-07 18:52:49 +0530

    Added the PageNotFound Page

---
+++ client/src/components/Pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PageNotFound() {
+    return (
+        <div className="h-[90vh] mx-auto w-full max-w-7xl flex justify-center items-center text-orange-600 text-xl lg:text-5xl md:text-4xl">
+            {" "}
+            404 Page Not Found <span className="animate-bounce text-3xl lg:text-5xl md:text-4xl ">🧐</span>
+        </div>
+    );
+}
+
+export default PageNotFound;
